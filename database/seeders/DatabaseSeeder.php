41cd23707231643d4ad01cd6fa71647f97cfaa2f
Mohammedwm <mmm2012012@hotmail.com>
2021-11-30 20:28:16 +0200

    d

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            UserSeeder::class,
-        ]);
+        \App\Models\Category::factory(6)->create();
+        \App\Models\Product::factory(22)->create();
     }
 }
